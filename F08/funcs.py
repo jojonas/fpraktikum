7253ea2878667536bf3b5d7ffe2ca17c6dc08f66
Jonas Lieb <lieb.jonas@gmail.com>
2015-03-20 13:12:31 +0100

    Funktionsbib

---
+++ F08/funcs.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+GAUSS = lambda x, mu=1, sigma=1, A=1: A*np.exp(-np.power(x-mu, 2)/(2*sigma**2))
+LINEAR = lambda x, slope=1, offset=0: slope*x + offset
+SQRT = lambda x, A=1, offset=0: A*np.sqrt(x)+offset
+CONSTANT = lambda x, c=0: c
+EXPONENTIAL_DECAY = lambda x, A, T, offset: A*np.exp(-x/T)+offset
+
+POLY0 = lambda x, a0=0: a0*np.ones_like(x)
+POLY1 = lambda x, a0=0, a1=1: a0 + x*a1
+POLY2 = lambda x, a0=0, a1=1, a2=1: a0 + x*a1 + np.power(x, 2)*a2
+POLY3 = lambda x, a0=0, a1=1, a2=1, a3=1: a0 + x*a1 + np.power(x, 2)*a2 + np.power(x, 3)*a3
+POLY4 = lambda x, a0=0, a1=1, a2=1, a3=1, a4=1: a0 + x*a1 + np.power(x, 2)*a2 + np.power(x, 3)*a3 + np.power(x, 4)*a4
